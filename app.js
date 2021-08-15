2fbc9ce81c1e07bc9e0a26705127a32b887b81d3
DragonPyli <codewizard001@gmail.com>
2021-08-15 14:31:07 +0530

    Added PORT

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var nStatic = require('node-static');
 
 var fileServer = new nStatic.Server('./static');
 
+console.log("[+] Server Starting");
+console.log(process.env.PORT||5000);
+
 http.createServer(function (req, res) {
     console.log('Request: ' + req.url);
     fileServer.serve(req, res);
-}).listen(5000);
+}).listen(process.env.PORT||5000);
