ad167c9aa0aa7b3e9d1c05b14583c10afe6d4332
DragonPyli <codewizard001@gmail.com>
2021-08-15 16:31:41 +0530

    Added Curtain

---
+++ static/player.js
@@ -0,0 +1,10 @@
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+
+var animation = lottieWeb.loadAnimation({
+  container: document.getElementById('container'),
+  path: 'svg/62717-confetti.json',
+  renderer: 'svg',
+  loop: true,
+  autoplay: false,
+  name: "Confetti Animation",
+});
